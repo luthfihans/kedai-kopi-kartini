91284b7d7143133bf5afa0c1d21cea069f67ed94
unknown <rohimpkb7@gmail.com>
2024-01-23 15:48:59 +0700

    membuat website kedai kopi kartini

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// toggle class active
+const navbarnav = document.querySelector(".navbar-nav");
+//ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarnav.classList.toggle("active");
+};
+
+
+
